d6fba5dbaa051075155f5aa5cf791774edaf703e
ellie-bellie <114118674+ellie-bellie@users.noreply.github.com>
2026-04-09 11:32:37 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'polish-palette-v2';
+const CACHE_NAME = 'polish-palette-v3';
 const urlsToCache = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', event => {
